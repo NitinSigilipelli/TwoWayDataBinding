3ae3b357be49b7824df2e7423b3e8ed988329e98
NitinSigilipelli <sigilipellinitin@gmail.com>
2023-08-09 22:30:44 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/twowaydatabinding/Person.java
@@ -0,0 +1,21 @@
+package com.example.twowaydatabinding;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+public class Person extends BaseObservable {
+    String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+    @Bindable
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+        notifyPropertyChanged(BR.name);
+    }
+}
